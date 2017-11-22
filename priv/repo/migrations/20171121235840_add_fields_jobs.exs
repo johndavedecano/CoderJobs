559f4b192204dc5def0c3abf45d4dd0728a2f035
John Dave Decano <johndavedecano@gmail.com>
2017-11-22 08:09:56 +0800

    Updated jobs database table added fields

---
+++ priv/repo/migrations/20171121235840_add_fields_jobs.exs
@@ -0,0 +1,11 @@
+defmodule Coderjobs.Repo.Migrations.AddFieldsJobs do
+  use Ecto.Migration
+
+  def change do
+    alter table(:jobs) do
+      add :tags, :string
+      add :salary, :string
+      add :is_remote, :boolean, default: false, null: false
+    end
+  end
+end
